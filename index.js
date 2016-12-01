8068a1d4860d43fdbbfb144ef752513feb54e83d
Muhammed Safa <muhammedsafabicakci@gmail.com>
2016-12-01 19:46:57 +0300

    express
    demo

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function (req, res) {
+   res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/hello', function (req, res) {
+   res.send('HelloWorld');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
